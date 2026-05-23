787107cfa96d7c053b607ee91b47ed079f4b16f3
davidgeraldm08 <davidgeraldm08@gmail.com>
2026-05-24 02:27:12 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+function goToLink(){
+    window.location.href = "https://www.facebook.com/geralddavid08"
+};
+
+function goToEmail(){
+    window.location.href = "mailto:davidgeraldm08@gmail.com"
+};
